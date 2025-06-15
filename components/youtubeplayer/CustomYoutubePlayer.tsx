f7e0340b65c8f51b2b1840c50eb9e7f9747a9f00
Pauline Dimmek <ma74reso@studserv.uni-leipzig.de>
2025-06-16 00:13:22 +0300

    some more styling

--- components/youtubeplayer/CustomYoutubePlayer.tsx
+++ components/youtubeplayer/CustomYoutubePlayer.tsx
@@ -7,18 +7,21 @@ type YouTubePlayerProps = {
 
 const CustomYouTubePlayer = ({ videoId }: YouTubePlayerProps) => {
   // Set up event handlers with proper types
-
-
   const onError: YouTubeProps['onError'] = (error) => {
     console.error('YouTube Player Error:', error);
   };
 
   return (
-    <YouTube
-      videoId={videoId}
-      onError={onError}
-      className="rounded-lg shadow-lg transition duration-500 group-hover:scale-105"
-    />
+    <div className="w-full relative" style={{ paddingTop: '56.25%' }}>
+      <div className="absolute top-0 left-0 w-full h-full">
+        <YouTube
+          videoId={videoId}
+          onError={onError}
+          className="w-full h-full rounded-lg shadow-lg transition duration-500 group-hover:scale-105"
+          iframeClassName="w-full h-full rounded-lg"
+        />
+      </div>
+    </div>
   );
 };
 
