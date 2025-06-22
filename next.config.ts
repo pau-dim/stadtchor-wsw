8ae4ca6f52f519bc9d74659ca34d82950190d8d9
Pauline Dimmek <ma74reso@studserv.uni-leipzig.de>
2025-06-22 23:03:52 +0300

    fix: include styles

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/stadtchor-wsw' : '',
-  assetPrefix: isProd ? '/stadtchor-wsw/' : '',
+  basePath: '/stadtchor-wsw',
+  assetPrefix: '/stadtchor-wsw',
   images: { unoptimized: true }, 
   /* config options here */
     eslint: {
